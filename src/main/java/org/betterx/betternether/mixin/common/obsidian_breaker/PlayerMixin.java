cb0b421759b08e86c1f7f41e23b7ebdd0838a1ed
Redmi <terminator4444_4@mail.ru>
2026-07-20 11:56:03 +0500

    fixed obsidian breaker + bump version

--- src/main/java/org/betterx/betternether/mixin/common/obsidian_breaker/PlayerMixin.java
+++ src/main/java/org/betterx/betternether/mixin/common/obsidian_breaker/PlayerMixin.java
@@ -2,7 +2,6 @@ package org.betterx.betternether.mixin.common.obsidian_breaker;
 
 import org.betterx.betternether.enchantments.ObsidianBreaker;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Player.class, remap = false)
 public class PlayerMixin {
-    @Inject(method = "getDestroySpeed(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)F", at = @At("RETURN"), cancellable = true, require = 0)
-    private void wover_test_getDestroySpeed(BlockState blockState, BlockPos pos, CallbackInfoReturnable<Float> cir) {
+    @Inject(method = "getDestroySpeed(Lnet/minecraft/world/level/block/state/BlockState;)F", at = @At("RETURN"), cancellable = true)
+    private void wover_test_getDestroySpeed(BlockState blockState, CallbackInfoReturnable<Float> cir) {
         final LivingEntity entity = (LivingEntity) (Object) this;
         cir.setReturnValue(ObsidianBreaker.modifyObsidianBreakerSpeed(blockState, cir.getReturnValue(), entity));
     }
