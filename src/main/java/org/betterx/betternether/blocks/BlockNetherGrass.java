13c4f6cd788edf1d2c5b0fd941bcdceb746199c2
Raijin <the_krap7@mail.ru>
2026-07-27 12:19:10 +0500

    bug fixes

--- src/main/java/org/betterx/betternether/blocks/BlockNetherGrass.java
+++ src/main/java/org/betterx/betternether/blocks/BlockNetherGrass.java
@@ -22,7 +22,6 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.util.random.Weighted;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
